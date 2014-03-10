9a289758598a8923cd654316c4fec459770115a6
Tom Mowad <tmowad@gmail.com>
2014-03-10 15:03:01 -0700

    Finish project in amateur scrappy mode, with AWS security credential script, and instructions for how to use it properly.

--- app/controllers/image_uploader_controller.rb
+++ app/controllers/image_uploader_controller.rb
@@ -7,8 +7,15 @@ class ImageUploaderController < ApplicationController
     if 
       @blah = "Yes, pic came in, class type '<b>#{pic.class}</b>'; content_type is #{pic.content_type}".html_safe
       rand = ('a'..'z').to_a.shuffle[0..7].join('')
-      imgwrite = File.open("#{pic.original_filename}_#{rand}", 'w')
-      imgwrite.write(pic.read.force_encoding('UTF-8'))
+      filename = "#{pic.original_filename}_#{rand}"
+      #imgwrite = File.open(filename, 'w')
+      #imgwrite.write(pic.read.force_encoding('UTF-8'))
+
+      s3 = AWS::S3.new
+      bucket_name = "img.demo.tmow2"
+      bucket = s3.buckets[bucket_name]
+      obj = bucket.objects[filename]
+      obj.write(pic.read)
     else
       @blah = "No pic here, sir!"
     end
